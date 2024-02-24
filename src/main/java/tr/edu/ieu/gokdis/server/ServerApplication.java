3f2fab839252526cae68adc54382de732f0d6447
eresos <145768339+eresos@users.noreply.github.com>
2024-02-24 14:47:59 +0300

    fix: configure CORS

--- src/main/java/tr/edu/ieu/gokdis/server/ServerApplication.java
+++ src/main/java/tr/edu/ieu/gokdis/server/ServerApplication.java
@@ -41,7 +41,7 @@ public class ServerApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
             }
         };
     }
