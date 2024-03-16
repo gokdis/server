f5c0315576275ea272a7c10506523b83b9662470
bilalsarac <bilalcan1999@hotmail.com>
2024-03-16 17:34:46 +0300

    fix: fix person register bug

--- src/main/java/eu/ecosys/gokdis/server/SecurityConfiguration.java
+++ src/main/java/eu/ecosys/gokdis/server/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package eu.ecosys.gokdis.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST,"/api/v1/person").permitAll()
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
                 .requiresChannel(requiresChannel -> requiresChannel.anyRequest().requiresSecure());
         return http.build();
