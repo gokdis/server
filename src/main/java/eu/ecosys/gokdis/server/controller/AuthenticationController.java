aefeb64dec207d0f0ccfdfb552fb1b9abb7c5d3f
eresos <145768339+eresos@users.noreply.github.com>
2024-05-02 16:56:36 +0300

    refactor: delete unnecessary imports

--- src/main/java/eu/ecosys/gokdis/server/controller/AuthenticationController.java
+++ src/main/java/eu/ecosys/gokdis/server/controller/AuthenticationController.java
@@ -1,20 +1,9 @@
 package eu.ecosys.gokdis.server.controller;
 
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-
-import eu.ecosys.gokdis.server.entity.Section;
-import eu.ecosys.gokdis.server.repository.SectionRepository;
 
 @RestController
 @RequestMapping("api/v1")
